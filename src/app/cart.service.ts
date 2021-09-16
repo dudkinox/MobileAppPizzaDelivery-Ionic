56b894cc5d4cb6a5162b4fdc9c36b0fd8b6b7d04
ศุภณัฐ ชินราช <45527907+dudkinox@users.noreply.github.com>
2021-09-16 16:30:51 +0700

    Work Jirayu

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,88 +2,108 @@ import { Injectable } from '@angular/core';
 import { Route } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-
   private data = [
     {
-      category: "Pizza",
+      category: 'Promotion',
       expended: true,
       products: [
-        {id: 0, name: 'ฮาวาเอี่ยน', price: '299', img: 'https://cdn.1112.com/1112/public/images/products/pizza/August2020/199203_2.png'},
-        {id: 1, name: 'แฮมและปูอัด', price: '299', img: 'https://nathaponweb.files.wordpress.com/2016/09/itm102226.jpg'},
-        {id: 2, name: 'อะโลฮ่า', price: '299', img: 'https://nathaponweb.files.wordpress.com/2016/09/itm1022261.jpg'},
-        {id: 3, name: 'แฮมและเห็ด', price: '299', img: 'https://nathaponweb.files.wordpress.com/2016/09/e0b981e0b8aee0b8a1e0b981e0b8a5e0b8b0e0b980e0b8abe0b987e0b894.jpg'},
-        {id: 4, name: 'เวจจี้', price: '299', img: 'https://nathaponweb.files.wordpress.com/2016/09/e0b980e0b8a7e0b888e0b888e0b8b5e0b989.jpg'},
-        {id: 5, name: 'ดับเบิ้ลชีสพิซซ่า', price: '299', img: 'https://nathaponweb.files.wordpress.com/2016/09/e0b894e0b8b1e0b89ae0b980e0b89ae0b8b4e0b989e0b8a5e0b88ae0b8b5e0b8aa.jpg'},
-        {id: 6, name: 'ดับเบิ้ลเป๊ปเปอโรนี', price: '299', img: 'https://nathaponweb.files.wordpress.com/2016/09/e0b894e0b8b1e0b89ae0b980e0b89ae0b8b4e0b989e0b8a5e0b980e0b89be0b98ae0b89be0b980e0b89be0b8ade0b982e0b8a3e0b899e0b8b5.jpg'},
-        {id: 7, name: 'ซีฟู้ดเดอลุกซ์', price: '369', img: 'https://nathaponweb.files.wordpress.com/2016/09/e0b980e0b894e0b8ade0b8a5e0b8b8e0b881e0b88be0b98c.jpg'},
-        {id: 8, name: 'ซีฟู้ดค็อกเทล', price: '369', img: 'https://nathaponweb.files.wordpress.com/2016/09/e0b884e0b987e0b8ade0b881e0b980e0b897e0b8a5.jpg'},
-        {id: 9, name: 'ค็อกเทลกุ้ง', price: '369', img: 'https://nathaponweb.files.wordpress.com/2016/09/e0b884e0b987e0b8ade0b881e0b980e0b897e0b8a5e0b881e0b8b8e0b989e0b887.jpg'},
-        {id: 10, name: 'ทรอปิคอลซีฟู้ด', price: '369', img: 'https://nathaponweb.files.wordpress.com/2016/09/e0b897e0b8a3e0b8ade0b89be0b8b4e0b884e0b8ade0b8a5e0b88be0b8b5e0b89fe0b8b9e0b989e0b894.jpg'},
-        {id: 11, name: 'ต้มยำกุ้ง', price: '369', img: 'https://nathaponweb.files.wordpress.com/2016/09/e0b895e0b989e0b8a1e0b8a2e0b8b3e0b881e0b8b8e0b989e0b887.jpg'}
-      ]
+        {
+          id: 0,
+          name: '2 Mango Berry Twin',
+          price: '210',
+          img: 'https://images-ext-2.discordapp.net/external/sYckUDkT15EDGeJsEYHhH8HoAgxU6ZT-Tc2mGeD9vyQ/%3Fwidth%3D600/https/images.deliveryhero.io/image/fd-th/Products/16020876.jpg',
+        },
+        {
+          id: 1,
+          name: '2 Coco Moo Moo',
+          price: '210',
+          img: 'https://images-ext-1.discordapp.net/external/5QkZ0SLBs26uuf-8L2uRslLS53QM-LLDxZKeUdrwQNk/%3Fwidth%3D600/https/images.deliveryhero.io/image/fd-th/Products/16020877.jpg',
+        },
+        {
+          id: 2,
+          name: '2 Berry Ghurt',
+          price: '210',
+          img: 'https://images-ext-1.discordapp.net/external/_EYD0vlpineKhh6vidxXlSs1vYynkE7ZRJuEu6Y5rsQ/%3Fwidth%3D600/https/images.deliveryhero.io/image/fd-th/Products/16020881.jpg',
+        },
+        {
+          id: 3,
+          name: '2 Berry the Boss',
+          price: '210',
+          img: 'https://images-ext-1.discordapp.net/external/r9gN_JEX0xvVsAiTk4XEh0fKC3jP5LkLvbaxmUzzW1A/%3Fwidth%3D600/https/images.deliveryhero.io/image/fd-th/Products/16020880.jpg',
+        },
+      ],
     },
     {
-      category: "PizzaPuf",
+      category: 'Super Fruit',
+      expended: true,
       products: [
-        {id: 12, jumbo: 'จัมโบ้ พิซซ่า พัฟ ', name: 'แฮมและปูอัด', price: '99', img: 'https://nathaponweb.files.wordpress.com/2016/10/itm102405.jpg'},
-        {id: 13, jumbo: 'จัมโบ้ พิซซ่า พัฟ ',name: 'ฮาวายเอี้ยน', price: '119', img: 'https://nathaponweb.files.wordpress.com/2016/10/itm102403.jpg'},
-        {id: 14, jumbo: 'จัมโบ้ พิซซ่า พัฟ ',name: 'ไก่สามรส', price: '119', img: 'https://nathaponweb.files.wordpress.com/2016/10/itm102404.jpg'},
-        {id: 15, jumbo: 'จัมโบ้ พิซซ่า พัฟ ',name: 'ซุปเปอร์เดอลุกซ์', price: '119', img: 'https://nathaponweb.files.wordpress.com/2016/10/itm102402.jpg'},
-        {id: 16, jumbo: 'จัมโบ้ พิซซ่า พัฟ ',name: 'ซีฟู้ดค็อกเทล', price: '139', img: 'https://nathaponweb.files.wordpress.com/2016/10/itm102401.jpg'}
-
-      ]
+        {
+          id: 4,
+          name: 'Homsuwan Detox',
+          price: '115',
+          img: 'https://images-ext-2.discordapp.net/external/JDLlfHeQRmRia1J4IM3q1ywgQBUQkuFcrd7M0soZp6E/%3Fwidth%3D600/https/images.deliveryhero.io/image/fd-th/Products/1861225.jpg',
+        },
+      ],
     },
     {
-      category: "Pasta",
+      category: 'BROWN SUGAR FRESH MILK',
+      expended: true,
       products: [
-        {id: 17, name: 'สปาเก็ตตี้ คาโบนาร่าอบ', price: '139', img: 'https://nathaponweb.files.wordpress.com/2016/09/e0b884e0b8b2e0b982e0b89ae0b899e0b8b2.jpg'},
-        {id: 18, name: 'สปาเก็ตตี้ ซอสไก่สับ', price: '99', img: 'https://nathaponweb.files.wordpress.com/2016/09/e0b984e0b881e0b988e0b8aae0b8b1e0b89a.jpg'},
-        {id: 19, name: 'สปาเก็ตตี้เบคอนผัดพริกแห้ง', price: '109', img: 'https://nathaponweb.files.wordpress.com/2016/09/e0b980e0b89ae0b884e0b8ade0b899.jpg'},
-        {id: 20, name: 'สปาเก็ตตี้ ขี้เมาทะเล', price: '139', img: 'https://nathaponweb.files.wordpress.com/2016/10/itm115580.jpg'},
-        {id: 21, name: 'สปาเก็ตตี้ใส้กรอกผัดพริกแห้ง', price: '99', img: 'https://nathaponweb.files.wordpress.com/2016/10/itm115536.jpg'},
-        {id: 22, name: 'สปาเก็ตตี้แฮมและเห็ด', price: '99', img: 'https://nathaponweb.files.wordpress.com/2016/10/itm115538.jpg'},
-        {id: 23, name: 'ผักโขมอบชีส', price: '179', img: 'https://nathaponweb.files.wordpress.com/2016/10/itm115505.jpg'}
-      ]
+        {
+          id: 5,
+          name: 'Brown Sugar Fresh Milk with Brown Sugar Jelly',
+          price: '69',
+          img: 'https://images-ext-2.discordapp.net/external/3BZmB1Xi4ka6-XnmZwht7cQaZYd5Hk6qsZxk-rf-uZs/%3Fwidth%3D600/https/images.deliveryhero.io/image/fd-th/Products/1893656.jpg',
+        },
+        {
+          id: 6,
+          name: 'Ceylon Tea with Brown Sugar Jelly',
+          price: '59',
+          img: 'https://images-ext-1.discordapp.net/external/taHOedMe-KDzM2DqGKHnfz5mhbSusZbtDYwb56RxN-k/%3Fwidth%3D600/https/images.deliveryhero.io/image/fd-th/Products/1893603.jpg',
+        },
+      ],
     },
     {
-      category: 'Salad',
+      category: 'Classic Smoothie',
       products: [
-        { id: 24, name: 'ซีซาร์สลัด', price: '139', img:'https://nathaponweb.files.wordpress.com/2016/10/itm116508.jpg'},
-        { id: 25, name: 'สลัดกุ้ง', price: '159', img: 'https://nathaponweb.files.wordpress.com/2016/10/itm116513.jpg' },
-        { id: 26, name: 'การ์เดนสลัด และน้ำสลัดเทาซันไอส์แลนด์', price: '99',img:'https://nathaponweb.files.wordpress.com/2016/10/itm116504.jpg' },
-        { id: 27, name: 'การ์เดนสลัด และน้ำครีมสลัด', price: '99',img:'https://nathaponweb.files.wordpress.com/2016/10/itm116503.jpg' }
-      ]
+        {
+          id: 7,
+          name: 'Berry Blitz',
+          price: '85',
+          img: 'https://images-ext-1.discordapp.net/external/IWtCb1WqaU0boUi9tG6JVx3AyZliu-DN66lrpzwR3Jc/%3Fwidth%3D600/https/images.deliveryhero.io/image/fd-th/Products/1861230.jpg',
+        },
+        {
+          id: 8,
+          name: 'Strawberry Sunrise',
+          price: '85',
+          img: 'https://images-ext-2.discordapp.net/external/qwqh_8hVbkw_Nilk5EcfSbIxqdYLPL9Gg-6uqIuL2zY/%3Fwidth%3D600/https/images.deliveryhero.io/image/fd-th/Products/1861231.jpg',
+        },
+        {
+          id: 9,
+          name: 'Mighty Mulberry',
+          price: '85',
+          img: 'https://images-ext-2.discordapp.net/external/7eQXryN0FX7nneGzW26wJWhlH6U5fFco1JZe4JMSAgA/%3Fwidth%3D600/https/images.deliveryhero.io/image/fd-th/Products/1861233.jpg',
+        },
+      ],
     },
-    {
-      category: 'Appetizer',
-      products: [
-        { id: 28, name: 'ชิกเก้น นักเก็ตส์ 6 ชิ้น พร้อมซอสมะเขือเทศ', price: '79', img:'https://nathaponweb.files.wordpress.com/2016/10/itm116569.jpg'},
-        { id: 29, name: 'ชิกเก้น นักเก็ตส์ 6 ชิ้น พร้อมน้ำจิ้มไก่', price: '79', img: 'https://nathaponweb.files.wordpress.com/2016/10/itm116570.jpg' },
-        { id: 30, name: 'ชิกเก้นสติ๊กส์', price: '89',img:'https://nathaponweb.files.wordpress.com/2016/10/itm1165201.jpg' },
-        { id: 31, name: 'ขนมปังกระเทียม', price: '79',img:'https://nathaponweb.files.wordpress.com/2016/10/itm116525.jpg' },
-        { id: 32, name: 'ขนมปังกระเทียมชีส', price: '89',img:'https://nathaponweb.files.wordpress.com/2016/10/itm116526.jpg' },
-        { id: 33, name: 'เบรดสติ๊กส์', price: '79',img:'https://nathaponweb.files.wordpress.com/2016/10/itm116527.jpg' }
-      ]
-    }
   ];
 
   private cart = [];
 
-  constructor() { }
+  constructor() {}
 
-  getProducts(){
+  getProducts() {
     return this.data;
   }
 
-  getCart(){
+  getCart() {
     return this.cart;
   }
 
-  addProduct(product){
+  addProduct(product) {
     this.cart.push(product);
   }
-
 }
--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,5 +1,6 @@
 import { CartService } from './../cart.service';
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cart',
@@ -7,12 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.page.scss'],
 })
 export class CartPage implements OnInit {
-
   selectedItems = [];
 
   total = 0;
 
-  constructor(private cartService: CartService) { }
+  constructor(
+    private cartService: CartService,
+    public alertController: AlertController
+  ) {}
 
   ngOnInit() {
     let items = this.cartService.getCart();
@@ -21,11 +24,29 @@ export class CartPage implements OnInit {
       if (selected[obj.id]) {
         selected[obj.id].count++;
       } else {
-        selected[obj.id] = {...obj, count: 1};
+        selected[obj.id] = { ...obj, count: 1 };
       }
     }
-    this.selectedItems = Object.keys(selected).map(key => selected[key])
-    this.total = this.selectedItems.reduce((a, b) => a + (b.count * b.price), 0);
+    this.selectedItems = Object.keys(selected).map((key) => selected[key]);
+    this.total = this.selectedItems.reduce((a, b) => a + b.count * b.price, 0);
   }
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'แจ้งเตือนลูกค้า',
+      message: 'จ่ายเงินสำเร็จ!!!!.',
+      buttons: ['ยอมรับ'],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  paySuccess() {
+    this.presentAlert();
+  }
 }
